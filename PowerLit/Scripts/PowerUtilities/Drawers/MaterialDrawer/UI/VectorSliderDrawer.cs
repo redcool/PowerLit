75971f3f9d965347b72fdf6f9cb2fe3f56cb2924
ken <redcool007@qq.com>
2022-04-15 11:06:46 +0800

    update VectorSliderDrawer.cs

--- PowerLit/Scripts/PowerUtilities/Drawers/MaterialDrawer/UI/VectorSliderDrawer.cs
+++ PowerLit/Scripts/PowerUtilities/Drawers/MaterialDrawer/UI/VectorSliderDrawer.cs
@@ -7,9 +7,15 @@ namespace PowerUtilities
     using UnityEditor;
     using UnityEngine;
     using System.Linq;
-
+    /// <summary>
+    /// Draw Vector on Material GUI
+    /// 
+    /// </summary>
     public class VectorSliderDrawer : MaterialPropertyDrawer
     {
+        const char ITEM_SPLITTER = ' ';
+        const char RANGE_SPLITTER = '_';
+
         const float LINE_HEIGHT = 18;
         string[] headers;
         Vector2[] ranges;
@@ -26,11 +32,11 @@ namespace PowerUtilities
         {
             if (!string.IsNullOrEmpty(headerString))
             {
-                headers = headerString.Split(' ');
+                headers = headerString.Split(ITEM_SPLITTER);
             }
             if (!string.IsNullOrEmpty(realRangeString))
             {
-                var rangeItems = realRangeString.Split(new[] { ' ', '_' }, StringSplitOptions.RemoveEmptyEntries);
+                var rangeItems = realRangeString.Split(new[] {ITEM_SPLITTER, RANGE_SPLITTER }, StringSplitOptions.RemoveEmptyEntries);
                 var halfLen = rangeItems.Length / 2;
                 ranges = new Vector2 [halfLen];
                 for (int i = 0; i < halfLen; i++)
@@ -70,16 +76,26 @@ namespace PowerUtilities
             }
         }
 
+        //float Lerp(float a, float b, float t) => a + (b - a) * t;
+        /// <summary>
+        /// Lerp revert
+        /// </summary>
+        /// <param name="min"></param>
+        /// <param name="max"></param>
+        /// <param name="t"></param>
+        /// <returns>[0,1]</returns>
+        float Remap(float min, float max, float t) => t / Mathf.Max(max - min, 0.0001f);
+
         private void DrawSliders(Rect position, ref Vector4 value)
         {
             var pos = new Rect(position.x, position.y, position.width, 18);
             for (int i = 0; i < headers.Length; i++)
             {
-                value[i] = EditorGUI.Slider(pos, headers[i], value[i], 0, 1);
+                value[i] = EditorGUI.Slider(pos, headers[i], Remap(ranges[i].x, ranges[i].y, value[i]), 0, 1);
                 pos.y += LINE_HEIGHT;
 
-                //if(ranges.Length == headers.Length)
-                    //value[i] = Mathf.Lerp(ranges[i].x,ranges[i].y, value[i]);
+                if (ranges != null && ranges.Length == headers.Length)
+                    value[i] = Mathf.Lerp(ranges[i].x, ranges[i].y, value[i]);
             }
         }
     }

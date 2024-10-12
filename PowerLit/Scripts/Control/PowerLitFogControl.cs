fd9669cf6be57a669bd120604de4b01c14206612
gamebox1 <redcool007@qq.com>
2024-10-12 19:13:06 +0800

    v2.2.35.5 add Sphere Fog Layers

--- PowerLit/Scripts/Control/PowerLitFogControl.cs
+++ PowerLit/Scripts/Control/PowerLitFogControl.cs
@@ -6,6 +6,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
+using System.Runtime.InteropServices;
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(PowerLitFogControl))]
@@ -13,7 +15,7 @@ public class PowerLitFogControlEditor : PowerEditor<PowerLitFogControl>
 {
     public override bool NeedDrawDefaultUI() => true;
 
-    public override string Version => "0.0.2";
+    public override string Version => "0.0.3";
 
     public override void DrawInspectorUI(PowerLitFogControl inst)
     {
@@ -22,6 +24,12 @@ public class PowerLitFogControlEditor : PowerEditor<PowerLitFogControl>
             inst.enabled = false;
             inst.enabled = true;
         }
+
+    }
+
+    public override void OnInspectorGUIChanged(PowerLitFogControl inst)
+    {
+        
     }
 }
 #endif
@@ -32,6 +40,9 @@ public class PowerLitFogControl : MonoBehaviour
     [Min(1)] public float updateInterval = 1;
     float lastTime;
 
+    [HelpBox]
+    public string helpBox = "this block will saved to sphereFogDatas[0]";
+    //================== default params ,keep
     [Tooltip("fogColor.rgb multiply fogColor.a")]
     public bool isFogColorApplyAlpha;
 
@@ -56,12 +67,14 @@ public class PowerLitFogControl : MonoBehaviour
 
     [Tooltip("noise appear out of this")]
     [Range(0.02f, 0.99f)] public float _FogNoiseStartRate = 0.1f;
-
-
     [Range(0, 1)] public float _FogNoiseIntensity = 1;
 
+    //================== sphereFogDatas 
+    public List<SphereFogData> sphereFogDatas = new List<SphereFogData>();
     // trace fog instances
     public static MonoInstanceManager<PowerLitFogControl> instanceManager = new MonoInstanceManager<PowerLitFogControl>();
+
+    GraphicsBuffer fogBuffer;
 
     // Start is called before the first frame update
     void Start()
@@ -71,15 +84,47 @@ public class PowerLitFogControl : MonoBehaviour
     private void OnEnable()
     {
         UpdateParams();
-        
     }
+
+    [CompileFinished]
+    static void OnCompileFinished()
+    {
+        instanceManager.InstanceList.ForEach(inst => inst.fogBuffer?.Dispose());
+    }
+    /// <summary>
+    /// save default fogParams to sphereFogDatas[0]
+    /// </summary>
+    private void SyncDefaultParamsToDatas()
+    {
+        var fogData = sphereFogDatas.Count > 0 ? sphereFogDatas[0] : new SphereFogData();
+        if (sphereFogDatas.Count == 0)
+            sphereFogDatas.Add(fogData);
+
+        fogData.isFogColorApplyAlpha = isFogColorApplyAlpha;
+        fogData._HeightFogMin = _HeightFogMin;
+        fogData._HeightFogMax = _HeightFogMax;
+        fogData._HeightFogMinColor = _HeightFogMinColor;
+        fogData._HeightFogMaxColor = _HeightFogMaxColor;
+        fogData._HeightFogFilterUpFace = _HeightFogFilterUpFace;
+        fogData._FogMin = _FogMin;
+        fogData._FogMax = _FogMax;
+        fogData._FogNearColor = _FogNearColor;
+        fogData._FogFarColor = _FogFarColor;
+        fogData._FogNoiseDir = _FogNoiseDir;
+
+        fogData._FogNoiseStartRate = _FogNoiseStartRate;
+        fogData._FogNoiseIntensity = _FogNoiseIntensity;
+    }
+
     private void OnDestroy()
     {
         instanceManager.Remove(this);
+        fogBuffer?.Dispose();
     }
 
     void LateUpdate()
     {
+        SyncDefaultParamsToDatas();
         TryUpdateParams();
     }
 
@@ -97,26 +142,79 @@ public class PowerLitFogControl : MonoBehaviour
     }
 
     // Update is called once per frame
-    public void UpdateParams()
+    public void UpdateSphereFog(SphereFogData fogData)
     {
-        Shader.SetGlobalFloat(nameof(_HeightFogMin), _HeightFogMin);
-        Shader.SetGlobalFloat(nameof(_HeightFogMax), _HeightFogMax);
-        Shader.SetGlobalInt(nameof(_HeightFogFilterUpFace), _HeightFogFilterUpFace ? 1 : 0);
+        if (fogData == null)
+            return;
 
-        Shader.SetGlobalColor(nameof(_FogNearColor), _FogNearColor* (isFogColorApplyAlpha ? _FogNearColor.a : 1));
-        //Shader.SetGlobalColor(nameof(_FogFarColor), _FogFarColor);
-        Shader.SetGlobalColor(nameof(_HeightFogMinColor), _HeightFogMinColor* (isFogColorApplyAlpha ? _HeightFogMinColor.a : 1));
-        Shader.SetGlobalColor(nameof(_HeightFogMaxColor), _HeightFogMaxColor* (isFogColorApplyAlpha ? _HeightFogMaxColor.a : 1));
+        Shader.SetGlobalFloat("_HeightFogMin", fogData._HeightFogMin);
+        Shader.SetGlobalFloat("_HeightFogMax", fogData._HeightFogMax);
+        Shader.SetGlobalInt("_HeightFogFilterUpFace", fogData._HeightFogFilterUpFace ? 1 : 0);
 
-        Shader.SetGlobalVector("_FogDistance", new Vector4(_FogMin, _FogMax));
-        Shader.SetGlobalVector("_FogNoiseTilingOffset", _FogNoiseDir);
-        Shader.SetGlobalVector("_FogNoiseParams", new Vector4(_FogNoiseStartRate, _FogNoiseIntensity));
+        Shader.SetGlobalColor("_FogNearColor", fogData._FogNearColor * (fogData.isFogColorApplyAlpha ? fogData._FogNearColor.a : 1));
+        //Shader.SetGlobalColor("_FogFarColor", _FogFarColor);
+        Shader.SetGlobalColor("_HeightFogMinColor", fogData._HeightFogMinColor * (fogData.isFogColorApplyAlpha ? fogData._HeightFogMinColor.a : 1));
+        Shader.SetGlobalColor("_HeightFogMaxColor", fogData._HeightFogMaxColor * (fogData.isFogColorApplyAlpha ? fogData._HeightFogMaxColor.a : 1));
 
-        RenderSettings.fogColor = _FogFarColor * (isFogColorApplyAlpha ? _FogFarColor.a : 1);
-        RenderSettings.fogStartDistance = _FogMin;
-        RenderSettings.fogEndDistance = _FogMax;
+        Shader.SetGlobalVector("_FogDistance", new Vector4(fogData._FogMin, fogData._FogMax));
+        Shader.SetGlobalVector("_FogNoiseTilingOffset", fogData._FogNoiseDir);
+        Shader.SetGlobalVector("_FogNoiseParams", new Vector4(fogData._FogNoiseStartRate, fogData._FogNoiseIntensity));
+
+        RenderSettings.fogColor = fogData._FogFarColor * (fogData.isFogColorApplyAlpha ? fogData._FogFarColor.a : 1);
+        RenderSettings.fogStartDistance = fogData._FogMin;
+        RenderSettings.fogEndDistance = fogData._FogMax;
 
         //RenderSettings.fog = _IsGlobalFogOn;
-        Shader.SetGlobalVector("_FogParams", new Vector4(0, 0, -1 / (_FogMax - _FogMin), _FogMax / (_FogMax - _FogMin)));
+        Shader.SetGlobalVector("_FogParams", new Vector4(0, 0, -1 / (fogData._FogMax - fogData._FogMin), fogData._FogMax / (fogData._FogMax - fogData._FogMin)));
+    }
+
+    public void UpdateParams()
+    {
+        Shader.SetGlobalInt("_SphereFogLayers", sphereFogDatas.Count);
+
+        // simpleFog
+        UpdateSimpleFogParams(sphereFogDatas[0]);
+
+        //========= strructedBuffer
+        UpdateStructuredBuffer();
+        //========= cbuffer
+        UpdateCBuffer();
+    }
+
+    void UpdateSimpleFogParams(SphereFogData fogData)
+    {
+        Shader.SetGlobalVector("_FogParams", new Vector4(0, 0, -1 / (fogData._FogMax - fogData._FogMin), fogData._FogMax / (fogData._FogMax - fogData._FogMin)));
+    }
+
+    private void UpdateStructuredBuffer()
+    {
+        if (fogBuffer == null || fogBuffer.count != sphereFogDatas.Count)
+        {
+            fogBuffer?.Dispose();
+
+            var stride = Marshal.SizeOf<SphereFogDataStruct>(); // 29 float
+            fogBuffer = new GraphicsBuffer(GraphicsBuffer.Target.Structured, sphereFogDatas.Count, stride);
+        }
+
+        fogBuffer.SetData(sphereFogDatas
+            .Select(d => (SphereFogDataStruct)d)
+            .ToArray());
+        Shader.SetGlobalBuffer("_SphereFogDatas", fogBuffer);
+    }
+
+    void UpdateCBuffer()
+    {
+        return;
+        Shader.SetGlobalFloatArray("_HeightFogMinArray", sphereFogDatas.Select(d => d._HeightFogMin).ToArray());
+        //_HeightFogMaxArray
+        //    _HeightFogMinColorArray
+        //    _HeightFogMaxColorArray
+
+        //    _HeightFogFilterUpFaceArray
+        //    _FogNearColorArrays
+        //    _FogDistanceArray
+        //    _FogNoiseTilingOffsetArray
+
+        //    _FogNoiseParamsArray
     }
 }
---
+++ PowerLit/Scripts/Control/SphereFogData.cs
@@ -0,0 +1,80 @@
+﻿using PowerUtilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+/// <summary>
+/// Show in inspector
+/// </summary>
+[Serializable]
+public class SphereFogData
+{
+    [Tooltip("fogColor.rgb multiply fogColor.a")]
+    public bool isFogColorApplyAlpha;
+
+    [Header("HeightFog")]
+    public float _HeightFogMin = 0;
+    public float _HeightFogMax = 50;
+    public Color _HeightFogMinColor = new Color(.5f, .5f, .5f, 1), _HeightFogMaxColor = Color.white;
+    public bool _HeightFogFilterUpFace;
+
+    [Header("DepthFog")]
+    public float _FogMin = 1;
+    public float _FogMax = 100;
+    public Color _FogNearColor = Color.black, _FogFarColor = Color.white;
+
+    [Header("Fog Noise")]
+    [DisplayName("FogNoise TilingOffset", "xy : worldPos tiling , zw:worldPos offset")]
+    //[ListItemDraw("x:,x,y:,y,z:,z,w:,w", "15,80,15,80,15,80,15,80",isShowTitleRow =true)]
+    public Vector4 _FogNoiseDir = new Vector4(0.4f, .3f, .2f, .1f);
+
+    [Tooltip("noise appear out of this")]
+    [Range(0.02f, 0.99f)] public float _FogNoiseStartRate = 0.1f;
+
+
+    [Range(0, 1)] public float _FogNoiseIntensity = 1;
+
+    public Vector4 FogParams() => new Vector4(0, 0, -1 / (_FogMax - _FogMin), _FogMax / (_FogMax - _FogMin));
+}
+/// <summary>
+/// Sync PowerShaderLib/SphereSogLib
+/// </summary>
+public struct SphereFogDataStruct
+{
+    public float heightFogMin;
+    public float heightFogMax;
+    public Vector4 heightFogMinColor;
+    public Vector4 heightFogMaxColor;
+    public float heightFogFilterUpFace;
+
+    public Vector4 fogNearColor;
+    public Vector4 fogFarColor;
+    public Vector2 fogDistance;
+    public Vector4 fogNoiseTilingOffset;
+    public Vector4 fogNoiseParams; // composite args
+    public Vector4 fogParams; // for SIMPLE_FOG
+
+    public static implicit operator SphereFogDataStruct(SphereFogData fogData)
+    {
+        return new SphereFogDataStruct
+        {
+            heightFogMin = fogData._HeightFogMin,
+            heightFogMax = fogData._HeightFogMax,
+            heightFogFilterUpFace = fogData._HeightFogFilterUpFace ? 1 : 0,
+
+            fogNearColor = fogData._FogNearColor * (fogData.isFogColorApplyAlpha ? fogData._FogNearColor.a : 1),
+            fogFarColor = fogData._FogFarColor * (fogData.isFogColorApplyAlpha ? fogData._FogFarColor.a : 1),
+
+            heightFogMinColor = fogData._HeightFogMinColor * (fogData.isFogColorApplyAlpha ? fogData._HeightFogMinColor.a : 1),
+            heightFogMaxColor = fogData._HeightFogMaxColor * (fogData.isFogColorApplyAlpha ? fogData._HeightFogMaxColor.a : 1),
+
+            fogDistance = new Vector4(fogData._FogMin, fogData._FogMax),
+            fogNoiseTilingOffset = fogData._FogNoiseDir,
+            fogNoiseParams = new Vector4(fogData._FogNoiseStartRate, fogData._FogNoiseIntensity),
+            fogParams = fogData.FogParams(),
+        };
+    }
+};
